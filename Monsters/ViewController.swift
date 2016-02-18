fe06bcf098df1379c0651b14acfcdbb26b705d18
Antonija <ipekica@gmail.com>
2016-02-18 18:00:11 +0100

    UI ready

--- Monsters/ViewController.swift
+++ Monsters/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Outlets
 
+    @IBOutlet weak var monsterImg: UIImageView!
+    
+    
+    // MARK: Properties
+    
+    var arrayOfImages = [UIImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for num in 1...4 {
+            let img = UIImage(named: "idle\(num).png")
+            if let img = img {
+                arrayOfImages.append(img)
+            }
+            
+        }
+        
+        monsterImg.animationImages = arrayOfImages
+        monsterImg.animationDuration = 0.8
+        monsterImg.animationRepeatCount = 0
+        monsterImg.startAnimating()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
